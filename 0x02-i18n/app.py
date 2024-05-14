8ef18aa09b99934465e55ca8e76fd06180b9e11e
Michael AFOLABI <117805721+Michafolab@users.noreply.github.com>
2024-05-14 18:31:50 +0100

    Update app.py
    App file

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+"""Creating a mock database
 """
-import pytz
-from typing import Union, Dict
-from flask_babel import Babel, format_datetime
 from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
 
 
-class Config:
-    """Represents a Flask Babel configuration.
-    """
+class Config(object):
+    """doc doc doc"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -17,8 +16,8 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -27,65 +26,56 @@ users = {
 }
 
 
-def get_user() -> Union[Dict, None]:
-    """Retrieves a user based on a user id.
-    """
-    login_id = request.args.get('login_as', '')
-    if login_id:
-        return users.get(int(login_id), None)
+def get_user():
+    """Return a user dict or None """
+    id = request.args.get('login_as')
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
     return None
 
 
 @app.before_request
-def before_request() -> None:
-    """Performs some routines before each request's resolution.
+def before_request():
+    """Function to execute before each is processed
+    it retrieves the user and assigns it to the global variable g.user
     """
     user = get_user()
     g.user = user
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    locale = query_table.get('locale', '')
-    if locale in app.config["LANGUAGES"]:
-        return locale
-    user_details = getattr(g, 'user', None)
-    if user_details and user_details['locale'] in app.config["LANGUAGES"]:
-        return user_details['locale']
-    header_locale = request.headers.get('locale', '')
-    if header_locale in app.config["LANGUAGES"]:
-        return header_locale
-    return app.config['BABEL_DEFAULT_LOCALE']
+def get_locale():
+    """ Return the locale pass in the agrument """
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @babel.timezoneselector
-def get_timezone() -> str:
-    """Retrieves the timezone for a web page.
-    """
-    timezone = request.args.get('timezone', '').strip()
-    if not timezone and g.user:
-        timezone = g.user['timezone']
+def get_timezone():
+    """Doc"""
     try:
-        return pytz.timezone(timezone).zone
+        if request.args.get('timezone'):
+            return pytz.timezone(request.args.get('timezone')).zone
+        if g.user and g.user.get('timezone'):
+            return pytz.timezone(g.user['timezone']).zone
     except pytz.exceptions.UnknownTimeZoneError:
-        return app.config['BABEL_DEFAULT_TIMEZONE']
+        pass
+    return 'UTC'
 
 
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    g.time = format_datetime()
-    return render_template('index.html')
+def index():
+    """Return the home page"""
+    from datetime import datetime
+    from flask_babel import format_datetime
+
+    current_time = format_datetime(datetime.utcnow())
+    return render_template('index.html', current_time=current_time)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
 
